3ef0dcf988fc70af426f675114498cdabd4efc0b
Krystian <95060333+Croosheck@users.noreply.github.com>
2023-05-22 16:54:07 +0200

    Animations for the fav button added

--- screens/main/restaurants/restaurantProfile/AddFavorite.js
+++ screens/main/restaurants/restaurantProfile/AddFavorite.js
@@ -1,14 +1,57 @@
-import { StyleSheet, View } from "react-native";
+import { StyleSheet } from "react-native";
 import IconButton from "../../../../components/IconButton";
+import Animated, {
+	useAnimatedStyle,
+	useSharedValue,
+	withSequence,
+	withTiming,
+} from "react-native-reanimated";
+import { useEffect } from "react";
 
 const BORDER_MARGINS = 20;
 const ICON_SIZE = 36;
 
 const AddFavorite = ({ onFavPress = () => {}, isFavorite = Boolean() }) => {
+	const animatedBackScale = useSharedValue(0);
+	const animatedScale = useSharedValue(0);
+
+	const reanimatedBackIconStyle = useAnimatedStyle(() => ({
+		transform: [{ scale: animatedBackScale.value }],
+	}));
+	const reanimatedIconStyle = useAnimatedStyle(() => ({
+		transform: [{ scale: animatedScale.value }],
+	}));
+
+	useEffect(() => {
+		animatedBackScale.value = withTiming(1, { duration: 300 });
+	}, []);
+
+	useEffect(() => {
+		if (!isFavorite) {
+			animatedScale.value = withTiming(1, { duration: 450 });
+		}
+
+		if (isFavorite) {
+			animatedScale.value = withSequence(
+				withTiming(1.4, { duration: 200 }),
+				withTiming(0.8, { duration: 200 }),
+				withTiming(1.1, { duration: 150 }),
+				withTiming(0.9, { duration: 150 }),
+				withTiming(1, { duration: 100 })
+			);
+		}
+	}, [isFavorite]);
+
 	const favIcon = isFavorite ? "heart" : "heart-outline";
 
 	return (
-		<View style={[styles.container, { opacity: isFavorite ? 1 : 0.7 }]}>
+		<Animated.View
+			style={[
+				styles.container,
+				{ opacity: isFavorite ? 1 : 0.7 },
+				reanimatedBackIconStyle,
+			]}
+		>
 			<IconButton
 				style={styles.iconBack}
 				icon="heart"
@@ -16,14 +59,16 @@ const AddFavorite = ({ onFavPress = () => {}, isFavorite = Boolean() }) => {
 				size={ICON_SIZE * 1.3}
 				pressable={false}
 			/>
-			<IconButton
-				style={styles.iconFront}
-				icon={favIcon}
-				size={ICON_SIZE}
-				color="#FF3F3F"
-				onPress={onFavPress}
-			/>
-		</View>
+			<Animated.View style={[reanimatedIconStyle]}>
+				<IconButton
+					style={styles.iconFront}
+					icon={favIcon}
+					size={ICON_SIZE}
+					color="#FF3F3F"
+					onPress={onFavPress}
+				/>
+			</Animated.View>
+		</Animated.View>
 	);
 };
 
